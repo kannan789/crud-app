3fcc4cd42dfa809cdb2c7accddc2f5a2a64b7bad
Kannan Duraisamy <currentkannan@gmail.com>
2026-03-06 17:32:07 +0530

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ListRendering from './assets/components/ListRendering'
+import ListRendering from './components/ListRendering'
 
 function App() {
   const [count, setCount] = useState(0)
